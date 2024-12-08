fbddb939559f8e88157fdde3d478b5a6816b1a6e
Ronnie van 't Westeinde <ronnie@westeindeweb.nl>
2024-12-08 16:13:04 +0100

    Finally fixed walking off the map in 6b

--- dec6.py
+++ dec6.py
@@ -6,6 +6,9 @@ import sys
 class LoopDetectedException(Exception):
     pass
 
+class WalkOffMapException(Exception):
+    pass
+
 class Dec6:
     posx = 0
     posy = 0
@@ -93,20 +96,29 @@ class Dec6:
 
     def step_towards(self, newx, newy):
         if newx < 0 or newx >= self.sizex or newy < 0 or newy >= self.sizey:
-            raise IndexError('Out of bounds')
+            raise WalkOffMapException('Out of bounds')
         self.posx, self.posy = newx, newy
 
     def mark_current_as_visited(self):
         if self.matrix[self.posy][self.posx] == '.':
             self.matrix[self.posy][self.posx] = 'X'  # 'X' if self.turns else '*'
 
+    def mark_current_as_visited_with_direction(self):
+        if self.matrix[self.posy][self.posx] == '.':
+            self.matrix[self.posy][self.posx] = self.direction
+
     def make_move(self, mark_path):
         newx, newy = self.calculate_new_position()
+        if newx < 0 or newx >= self.sizex or newy < 0 or newy >= self.sizey:
+            # Walk off the map
+            raise WalkOffMapException()
         if self.matrix[newy][newx] in '#O':
             self.rotate_guard()
         else:
             self.step_towards(newx, newy)
-            if mark_path:
+            if mark_path == 'special':
+                self.mark_current_as_visited_with_direction()
+            elif mark_path:
                 self.mark_current_as_visited()
 
     def print(self):
@@ -119,7 +131,7 @@ class Dec6:
         while True:
             try:
                 self.make_move(mark_path=mark_path)
-            except IndexError:
+            except WalkOffMapException:
                 return False
             except LoopDetectedException:
                 return True
@@ -138,6 +150,46 @@ class Dec6:
         self.walk(mark_path=True)
         return self.count_visited()
 
+    def try_scenario_with_obstacle(self, x, y, mark_path=False):
+        if self.visit_matrix[y][x] != 'X':
+            return False
+
+        obstacle_creates_loop = False
+        self.restore_matrix()
+        orig_content = self.matrix[y][x]
+        self.matrix[y][x] = 'O'
+        if self.walk(mark_path=mark_path):
+            obstacle_creates_loop = True
+        # Restore
+        self.matrix[y][x] = orig_content
+        return obstacle_creates_loop
+
+
+    def analyze_two_positions(self, input):
+        self.matrix = self.parse_input(input)
+        self.determine_position()
+        self.walk(mark_path=True)
+        self.visit_matrix = copy.deepcopy(self.matrix)
+
+        self.matrix = self.parse_input(input)
+        self.determine_position()
+        self.store_orig()
+        # (97, 24), (97, 27)
+        # Valid: 97, 41
+
+        x, y = 97, 41
+        loop = self.try_scenario_with_obstacle(x, y)
+        print(x, y, loop)
+
+        x, y = 97, 24
+        loop = self.try_scenario_with_obstacle(x, y, mark_path=True)
+        print(x, y, loop)
+        self.print()
+
+        # self.matrix[24][97] = '*'
+        # self.matrix[27][97] = '*'
+        # self.print()
+
     def solve2(self, input, print_output=False):
         # Use the X in solution 1 to get to faster results
         self.matrix = self.parse_input(input)
@@ -155,29 +207,12 @@ class Dec6:
         # percentage = 0
         # if self.show_progress:
         #     print()
-        # prev_pos_x, prev_pos_y = None, None
         for y in range(self.sizey):
             for x in range(self.sizex):
-                # if prev_pos_x is not None:
-                #     self.matrix[prev_pos_y][prev_pos_x] = 'X'
-                if (x, y) not in [(97, 24), (97, 27)]:
-                    continue
-                if self.visit_matrix[y][x] == 'X':
-                    self.restore_matrix()
-                    self.matrix[y][x] = 'O'
-                    if self.walk(mark_path=False):
-                        obstacle_positions.add((x,y))
-                        self.print()
-                        print()
+                if self.try_scenario_with_obstacle(x, y):
+                    obstacle_positions.add((x, y))
+                    obstacles_that_make_loops += 1
 
-                        obstacles_that_make_loops += 1
-                        # if print_output:
-                        #     print()
-                        #     self.print()
-                        #     print()
-                    self.matrix[y][x] = 'X'
-                    # prev_pos_x, prev_pos_y = x, y
-        #         count += 1
         #         if int(count*100/total) > percentage:
         #             percentage = int(count*100/total)
         #             if self.show_progress:
@@ -224,7 +259,7 @@ class Test(unittest.TestCase):
     def test_solution1(self):
         with open('dec6.txt', 'r') as f:
             output = self.solver.solve1(f.read())
-            self.solver.print()
+            # self.solver.print()
             self.print(output)
             self.assertEqual(output, 4883)
 
@@ -242,5 +277,11 @@ class Test(unittest.TestCase):
             self.assertLess(output, 1657)      # Wrong answer
             self.assertEqual(output, 1655)     # Correct answer
             self.assertNotEqual(output, 1652)  # Wrong answer (4th time)
+
+    def test_analyze(self):
+        with open('dec6.txt', 'r') as f:
+            output = self.solver.analyze_two_positions(f.read())
+            self.print(output)
+
 
 unittest.main()
