9bd89fc034cf91538cd214e8e1546d5e86bbd96e
Ronnie van 't Westeinde <ronnie@westeindeweb.nl>
2024-12-08 17:52:57 +0100

    run all testcases at once by run_all.py

--- run_all.py
+++ run_all.py
@@ -1,10 +1,3 @@
-# from dec1 import *
-# from dec2 import *
-# from dec3 import *
-# from dec4 import *
-# from dec5 import *
-# from dec6 import *
-import importlib
 import os
 import unittest
 
@@ -12,8 +5,6 @@ suite = unittest.TestSuite()
 for filename in sorted(os.listdir('.')):
     if filename.startswith('dec') and filename.endswith('.py'):
         name = filename.replace('.py', '')
-        # print(name)
-        # module = importlib.import_module(name)
         suite.addTests(unittest.defaultTestLoader.loadTestsFromName(name))
 
 runner = unittest.TextTestRunner(verbosity=2)
