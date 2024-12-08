61e59cd1dd6d33903d09ecbf4c0dbf2d3805f6a7
Ronnie van 't Westeinde <ronnie@westeindeweb.nl>
2024-12-08 07:12:40 +0100

    Check in on dec8

--- dec8.py
+++ dec8.py
@@ -1,60 +1,57 @@
-import copy
 import unittest
 import sys
 
 class Dec8:
     coordinates = []
+    sizex = 0
+    sizey = 0
 
-    def parse_input(self, input):
+    @staticmethod
+    def parse_input(inp):
         matrix = []
-        for line_ in input.strip().splitlines():
+        for line_ in inp.strip().splitlines():
             if line_.strip():
                 matrix.append(line_.strip())
         return matrix
 
-    def convert_map_to_coordinates(self, map):
+    def convert_map_to_coordinates(self, matrix):
         self.coordinates = []
-        for r, row in enumerate(map):
+        for r, row in enumerate(matrix):
             for c, signal in enumerate(row):
                 if signal != '.':
                     self.coordinates.append((c, r, signal))
-        self.sizex = len(map[0])
-        self.sizey = len(map)
+        self.sizex = len(matrix[0])
+        self.sizey = len(matrix)
 
-    def solve1(self, input):
-        matrix = self.parse_input(input)
-        self.convert_map_to_coordinates(matrix)
+    def create_antinodes(self, freq_range):
         antinodes = set()
         for c1 in self.coordinates:
             for c2 in self.coordinates:
                 if c1[2] == c2[2] and c1[0:2] != c2[0:2]:
-                    # Create two antinodes
-                    antinode1 = 2*c1[0]-c2[0], 2*c1[1]-c2[1]
-                    antinode2 = 2*c2[0]-c1[0], 2*c2[1]-c1[1]
-                    if 0 <= antinode1[0] < self.sizex and 0 <= antinode1[1] < self.sizey:
-                        antinodes.add(antinode1)
-                    if 0 <= antinode2[0] < self.sizex and 0 <= antinode2[1] < self.sizey:
-                        antinodes.add(antinode2)
-        return len(antinodes)
-
-    def solve2(self, input):
-        matrix = self.parse_input(input)
-        self.convert_map_to_coordinates(matrix)
-        antinodes = set()
-        for c1 in self.coordinates:
-            for c2 in self.coordinates:
-                if c1[2] == c2[2] and c1[0:2] != c2[0:2]:
-                    for freq in range(-self.sizex, self.sizex):
-                        # Create antinodes
+                    for freq in freq_range:
                         antinode = c1[0]+freq*(c2[0]-c1[0]), c1[1]+freq*(c2[1]-c1[1])
                         if 0 <= antinode[0] < self.sizex and 0 <= antinode[1] < self.sizey:
                             antinodes.add(antinode)
+        return antinodes
+
+    def solve1(self, inp):
+        matrix = self.parse_input(inp)
+        self.convert_map_to_coordinates(matrix)
+        antinodes = self.create_antinodes([-1, 2])
+        return len(antinodes)
+
+    def solve2(self, inp):
+        matrix = self.parse_input(inp)
+        self.convert_map_to_coordinates(matrix)
+        antinodes = self.create_antinodes(range(-self.sizex, self.sizex))
         return len(antinodes)
 
 
-# --------------------
+# ****************************************************************************
+# Unittests
+# ****************************************************************************
 
-class Test(unittest.TestCase):
+class TestDec8(unittest.TestCase):
     EXAMPLE_INPUT = """
     ............
     ........0...
@@ -72,6 +69,10 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.solver = Dec8()
 
+    def print(self, output):
+        print(output, end=' ')
+        sys.stdout.flush()
+
     def test_example1(self):
         output = self.solver.solve1(self.EXAMPLE_INPUT)
         self.assertEqual(output, 14)
@@ -79,7 +80,7 @@ class Test(unittest.TestCase):
     def test_solution1(self):
         with open('dec8.txt', 'r') as f:
             output = self.solver.solve1(f.read())
-            print(output)
+            self.print(output)
             self.assertEqual(output, 359)
 
     def test_example2(self):
@@ -89,7 +90,8 @@ class Test(unittest.TestCase):
     def test_solution2(self):
         with open('dec8.txt', 'r') as f:
             output = self.solver.solve2(f.read())
-            print(output)
-            self.assertEqual(output, 1293)     # Correct answer
+            self.print(output)
+            self.assertEqual(output, 1293)
+
 
 unittest.main()
---
+++ dec9.py
@@ -0,0 +1,71 @@
+import unittest
+import sys
+
+
+class Dec9:
+    def parse_input(self, inp):
+        matrix = []
+        for line_ in inp.strip().splitlines():
+            if line_.strip():
+                matrix.append(line_.strip())
+        return matrix
+
+    def solve1(self, inp):
+        matrix = self.parse_input(inp)
+        return 0
+
+    def solve2(self, inp):
+        matrix = self.parse_input(inp)
+        return 0
+
+
+# ****************************************************************************
+# Unittests
+# ****************************************************************************
+
+class TestDec9(unittest.TestCase):
+    EXAMPLE_INPUT = """
+    ............
+    ........0...
+    .....0......
+    .......0....
+    ....0.......
+    ......A.....
+    ............
+    ............
+    ........A...
+    .........A..
+    ............
+    ............
+    """
+
+    def setUp(self):
+        self.solver = Dec9()
+
+    @staticmethod
+    def print(output):
+        print(output, end=' ')
+        sys.stdout.flush()
+
+    def test_example1(self):
+        output = self.solver.solve1(self.EXAMPLE_INPUT)
+        self.assertEqual(output, 14)
+
+    # def test_solution1(self):
+    #     with open('dec9.txt', 'r') as f:
+    #         output = self.solver.solve1(f.read())
+    #         self.print(output)
+    #         self.assertEqual(output, 359)
+    #
+    # def test_example2(self):
+    #     output = self.solver.solve2(self.EXAMPLE_INPUT)
+    #     self.assertEqual(output, 34)
+    #
+    # def test_solution2(self):
+    #     with open('dec9.txt', 'r') as f:
+    #         output = self.solver.solve2(f.read())
+    #         self.print(output)
+    #         self.assertEqual(output, 1293)
+
+
+unittest.main()
