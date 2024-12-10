59e635047bc752b0676354a328ce671eea4fe010
Ronnie van 't Westeinde <ronnie@westeindeweb.nl>
2024-12-10 19:20:15 +0100

    Automatically download input data

--- create_template.py
+++ create_template.py
@@ -1,7 +1,9 @@
 import datetime
 import os
+import requests
 
 day = datetime.date.today().day
+year = datetime.date.today().year
 
 TEMPLATE = f"""import unittest
 import sys
@@ -40,38 +42,44 @@ class TestDec{day}(unittest.TestCase):
         print(output, end=' ')
         sys.stdout.flush()
 
+    @staticmethod
+    def get_input():
+        with open('dec{day}.txt', 'r') as f:
+            return f.read()
+
     def test_example1(self):
         output = self.solver.solve1(self.EXAMPLE_INPUT)
         self.assertEqual(output, 0)
 
     # def test_solution1(self):
-    #     with open('dec{day}.txt', 'r') as f:
-    #         output = self.solver.solve1(f.read())
-    #         self.print(output)
-    #         self.assertEqual(output, 0)
+    #     output = self.solver.solve1(self.get_input())
+    #     self.print(output)
     #
     # def test_example2(self):
     #     output = self.solver.solve2(self.EXAMPLE_INPUT)
     #     self.assertEqual(output, 0)
     #
     # def test_solution2(self):
-    #     with open('dec{day}.txt', 'r') as f:
-    #         output = self.solver.solve2(f.read())
-    #         self.print(output)
-    #         self.assertEqual(output, 0)
+    #     output = self.solver.solve2(self.get_input())
+    #     self.print(output)
 
 
 if __name__ == '__main__':
     unittest.main()
 """
 
-py_file = f'dec{day}.py'
-inp_file = f'dec{day}.txt'
+# Get the token by looking at session Cookies
+token = open('.token.txt').read()
 
+py_file = f'dec{day}.py'
 if not os.path.exists(py_file):
     with open(py_file, 'w') as f:
         f.write(TEMPLATE)
 
+inp_file = f'dec{day}.txt'
 if not os.path.exists(inp_file):
-    with open(inp_file, 'w') as f:
-        f.write('')
+    response = requests.get(f'https://adventofcode.com/{year}/day/{day}/input', headers={'Cookie': f'session={token}'})
+    print(response.text)
+    if response.status_code == 200:
+        with open(inp_file, 'w') as f:
+            f.write(response.text)
